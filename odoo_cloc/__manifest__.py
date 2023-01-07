432aefaf629dcca878c15d358fc98a8ee04d6199
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2023-01-06 22:39:39 -0600

    [ADD] repository initialization

---
+++ odoo_cloc/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Jarsa
+# License LGPL-3 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Odoo CLOC Webhook",
+    "summary": """
+    Add Webhook to get the Lines of Code in Odoo custom modules installed in database.
+    """,
+    "author": "Jarsa Sistemas",
+    "website": "https://www.jarsa.com",
+    "license": "LGPL-3",
+    "category": "Installer",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "base",
+    ],
+    "installable": True,
+}
